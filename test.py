f83488a53ddfe84a533a912000a33b81825ebb0a
ddijk <dick@dijkrosoft.nl>
2020-12-11 20:33:49 +0100

    Add unit test

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""tests for gitlog.py"""
+
+import os
+from subprocess import getstatusoutput, getoutput
+
+prg = './gitlog.py'
+
+def test_a():
+    rv, out = getstatusoutput(f'{prg} lijst')
+    # print(out)
+    assert rv == 0
+    expected= ('https://jira.ontwikkel.local/browse/HHOPPDEV-12345\n'
+               'https://jira.ontwikkel.local/browse/HHOPPDEV-7722')
+    assert out.strip() == expected
