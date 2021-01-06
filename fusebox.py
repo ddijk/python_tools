25d97e0eb3f9e29006156d4e4808e8e28a3cef28
ddijk <dick@dijkrosoft.nl>
2021-01-06 10:54:52 +0100

    refactored to more functional style

--- fusebox.py
+++ fusebox.py
@@ -9,37 +9,35 @@ import argparse
 import requests
 import re
 import json
-from io import StringIO
-
 
 # --------------------------------------------------
+
 def main():
-    response = requests.get('https://www.thenorthface.nl/shop/nl/tnf-nl/base-camp-fuse-box-3kvr', stream=True)
+    response = requests.get(
+        'https://www.thenorthface.nl/shop/nl/tnf-nl/base-camp-fuse-box-3kvr', stream=True)
+    price_lines = filter(lambda s: s.find('itemPrices')
+                         != -1, map(bytes.decode, response.iter_lines()))
+
+    list(map(checkPrices, map(capturePrices, price_lines)))
+
+
+def capturePrices(line):
     pattern = r'var itemPrices = ({.*});$'
-    n = 0;
-    for line in response.iter_lines():
-        n += 1
-        s = line.decode() 
-        f =  s.find('itemPrices')
-        if f != -1:
-            m =  re.search(pattern, s)
-            if m:
-                 prices = json.load(StringIO(m.group(1)))
+    return re.search(pattern, line)
+
+
+def checkPrices(match):
+    prices = json.loads(match.group(1))
 #                 for color in [{"c": "ASPHALT GREY/SUMMIT GOLD", "p": 130},{"c":"TNF BLACK", "p": 104}, {"c":"Wrought Iron-Tnf Black", "p": 130}]:
-                 for color in [{"c": "ASPHALT GREY/SUMMIT GOLD", "p": 130},{"c":"TNF BLACK", "p": 104} ]:
-                     print('-----------')
-                     print(f'kleur: {color["c"]}')
-                     handle(prices, color["c"], color["p"])
+    for product in [{"color": "ASPHALT GREY/SUMMIT GOLD", "price": 130}, {"color": "TNF BLACK", "price": 104}]:
+        print('-----------')
+        print(f'kleur: {product["color"]}')
+        currentPrice = prices['713173']['pricing']['7000000000000073250'][product["color"]]['highPriceNumeric']
+        if currentPrice < product["price"]:
+            print(f'PRIJS IS GEDAALD: {currentPrice}')
+        if currentPrice == product["price"]:
+            print(f'prijs is gelijk: {currentPrice}')
 
-
-def handle(prices, color, previousPrice):
-     currentPrice = (prices['713173']['pricing']['7000000000000073250'][color]['highPriceNumeric'])
-    
-     if currentPrice < previousPrice:
-         print(f'PRIJS IS GEDAALD: {currentPrice}') 
-     if currentPrice == previousPrice:
-         print(f'prijs is gelijk: {currentPrice}') 
-    
 
 # --------------------------------------------------
 if __name__ == '__main__':
