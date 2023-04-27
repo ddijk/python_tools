e3254885036c120843aebdc5eb50e2851651f72d
Dick Dijk <dick@dijkrosoft.nl>
2023-04-27 19:17:01 +0200

    tussenversie

--- knwu_punten.py
+++ knwu_punten.py
@@ -1,30 +1,44 @@
 import sys
-from knwu import login, url
+from knwu import login 
 
 def main():
-    if len(sys.argv)<3:
+    if len(sys.argv)<2:
         print("Geef als eerste arg de naam van de renner, en als tweede het oude aantal punten")
         sys.exit()
 
     naam = sys.argv[1]
-    punten = int(sys.argv[2])
-    print(f'Kijken of {naam} nog steeds {punten} punten heeft: ', end='')
+
+    if len(sys.argv)>2:
+        punten = int(sys.argv[2])
+        print(f'Kijken of {naam} nog steeds {punten} punten heeft: ', end='')
+    else:
+        punten = 0
 
     huidige_aantal = get_punten(naam)
 
-    print('GEWIJZIGD') if punten != huidige_aantal else print(f'geen update, nog steeds {huidige_aantal}')
+    if huidige_aantal > 0 and punten:
+        print('GEWIJZIGD') if punten != huidige_aantal else print(f'geen update, nog steeds {huidige_aantal}')
 
 def get_punten(naam):
     with open('credentials') as creds:
        session, cookies3, proxy = login(creds) 
     headers = { 'Accept' : 'application/json'}
 
-    klassement_url = 'https://mijn.knwu.nl/api/competitions/55/results?filter[competition]=55&filter[year]=2023&filter[race]=70695&filter[mode]=rank&filter[rank]=General&filter[user.name]=herw&include[0]=user&include[1]=license.organisation&permissions[0]=export&page=1&view=list'
+    klassement_url = f'https://mijn.knwu.nl/api/competitions/55/results?filter[competition]=55&filter[year]=2023&filter[race]=70695&filter[mode]=rank&filter[rank]=General&filter[user.name]={naam}&include[0]=user&include[1]=license.organisation&permissions[0]=export&page=1&view=list'
     klassement = session.get(f'{klassement_url}', cookies=cookies3, proxies=proxy, headers=headers)
     renners = klassement.json()
-    n = renners['data'][0]['points']
-    #print(f'aantal punten {n}')
-    return int(n)
+    if not len(renners['data']):
+        print(f'{naam} is niet bekend')
+    else:
+        if len(renners['data'])==1: 
+            n = renners['data'][0]['points']
+            return int(n)
+        else:
+            print('Meerdere renners gevonden:')
+            for r in renners['data']:
+                print(f"renner {r['user']['name']} heeft {r['points']} punten")
+    # print(f'aantal punten {n}')
+    return -1
 
 
 
